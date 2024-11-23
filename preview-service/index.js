aac518c5e1155ce02ea84700d4df25505c65152e
Chhawinderpreet Singh <96365487+chhawinder@users.noreply.github.com>
2024-11-23 13:54:34 +0400

    Update index.js

--- preview-service/index.js
+++ preview-service/index.js
@@ -6,7 +6,16 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const allowedDomain = 'https://rapidford-5sgy.vercel.app/';
+
+// Configure CORS
+app.use(cors({
+    origin: allowedDomain, // Only allow this domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Headers allowed in requests
+    credentials: true // Enable credentials sharing if needed
+}));
+// Middleware setup
 app.use(express.json());
 
 app.use("/preview", previewRoute);
