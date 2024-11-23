3300ccd1108ce1758c8e61c635acbf202387123c
Chhawinderpreet Singh <96365487+chhawinder@users.noreply.github.com>
2024-11-23 13:54:10 +0400

    Update index.js

--- encrypted-convert-service/index.js
+++ encrypted-convert-service/index.js
@@ -7,8 +7,16 @@ dotenv.config(); // Load environment variables from the .env file
 
 const app = express();
 
+const allowedDomain = 'https://rapidford-5sgy.vercel.app/';
+
+// Configure CORS
+app.use(cors({
+    origin: allowedDomain, // Only allow this domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Headers allowed in requests
+    credentials: true // Enable credentials sharing if needed
+}));
 // Middleware setup
-app.use(cors()); // Enable CORS for all origins (optional)
 app.use(express.json()); // Parse incoming JSON requests
 
 console.log("working...");
